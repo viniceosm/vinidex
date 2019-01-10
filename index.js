601c72feafe1606afdd7afa8c74998078a3a11e8
viniceosm <miiller.vini@gmail.com>
2019-01-10 07:55:16 -0200

    Updated indent mode, indent size and name DB

--- index.js
+++ index.js
@@ -1,99 +1,101 @@
 class Vinidex {
-	init(nameDb) {
-		let self = this;
+  init(nameDb) {
+    let self = this;
 
-		return new Promise(function (resolve, reject) {
-			if (!window.indexedDB) {
-				console.log("Seu navegador não suporta o recurso IndexedDB");
-				return;
-			}
+    return new Promise(function (resolve, reject) {
+      if (!window.indexedDB) {
+        console.log("Seu navegador não suporta o recurso IndexedDB");
+        return;
+      }
 
-			let request = window.indexedDB.open(nameDb, 2);
+      let request = window.indexedDB.open(nameDb, 3);
 
-			request.onerror = function (event) {
-				console.log("Erro ao abrir o banco de dados", event);
-			}
+      request.onerror = function (event) {
+        console.log("Erro ao abrir o banco de dados", event);
+        reject();
+      }
 
-			request.onupgradeneeded = function (event) {
-				console.log("Atualizando...");
-				self.db = event.target.result;
-				let objectStore = self.db.createObjectStore("Estudantes", { keyPath: "codigo" });
-			};
+      request.onupgradeneeded = function (event) {
+        console.log("Atualizando...");
+        self.db = event.target.result;
+        let objectStore = self.db.createObjectStore("Estudantes", { keyPath: "codigo" });
+        resolve();
+      };
 
-			request.onsuccess = function (event) {
-				console.log("Banco de dados aberto com sucesso.");
-				self.db = event.target.result;
-				resolve();
-			}
-		});
-	}
+      request.onsuccess = function (event) {
+        console.log("Banco de dados aberto com sucesso.");
+        self.db = event.target.result;
+        resolve();
+      }
+    });
+  }
 
-	trans(arrObjectStore, mode = 'readwrite') {
-		let transaction = this.db.transaction(arrObjectStore, mode);
+  trans(arrObjectStore, mode = 'readwrite') {
+    let transaction = this.db.transaction(arrObjectStore, mode);
 
-		return transaction;
-	}
+    return transaction;
+  }
 
-	add(nameObjectStore, valueAdd) {
-		let self = this;
-		return new Promise(function (resolve, reject) {
-			let transaction = self.trans([nameObjectStore]);
+  add(nameObjectStore, valueAdd) {
+    let self = this;
+    return new Promise(function (resolve, reject) {
+      let transaction = self.trans([nameObjectStore]);
 
-			transaction.oncomplete = function (event) {
-				console.log("Adicionado com Sucesso");
-				resolve();
-			};
+      transaction.oncomplete = function (event) {
+        console.log("Adicionado com Sucesso");
+        resolve();
+      };
 
-			transaction.onerror = function (event) {
-				console.log("Erro ao adicionar", event);
-				reject();
-			};
+      transaction.onerror = function (event) {
+        console.log("Erro ao adicionar", event);
+        reject();
+      };
 
-			let objectStore = transaction.objectStore(nameObjectStore);
-			objectStore.add(valueAdd);
-		});
-	}
+      let objectStore = transaction.objectStore(nameObjectStore);
+      objectStore.add(valueAdd);
+    });
+  }
 
-	delete(nameObjectStore, codigo) {
-		let self = this;
-		return new Promise(function (resolve, reject) {
-			self.trans([nameObjectStore]).objectStore(nameObjectStore).delete(codigo);
-			resolve();
-		});
-	}
+  delete(nameObjectStore, codigo) {
+    let self = this;
+    return new Promise(function (resolve, reject) {
+      self.trans([nameObjectStore]).objectStore(nameObjectStore).delete(codigo);
+      resolve();
+    });
+  }
 
-	select(nameObjectStore, codigo) {
-		let self = this;
-		return new Promise(function (resolve, reject) {
-			let request = self.trans([nameObjectStore]).objectStore(nameObjectStore).get(codigo);
-			request.onsuccess = function (event) {
-				resolve(request.result);
-			};
-		});
-	}
+  select(nameObjectStore, codigo) {
+    let self = this;
+    return new Promise(function (resolve, reject) {
+      let request = self.trans([nameObjectStore]).objectStore(nameObjectStore).get(codigo);
+      request.onsuccess = function (event) {
+        resolve(request.result);
+      };
+    });
+  }
 
-	alter(nameObjectStore, codigo, atributos) {
-		let self = this;
+  alter(nameObjectStore, codigo, atributos) {
+    let self = this;
 
-		let transaction = self.trans([nameObjectStore]);
-		let objectStore = transaction.objectStore(nameObjectStore);
-		let request = objectStore.get(codigo);
+    let transaction = self.trans([nameObjectStore]);
+    let objectStore = transaction.objectStore(nameObjectStore);
+    let request = objectStore.get(codigo);
 
-		return new Promise(function (resolve, reject) {
-			request.onsuccess = function(event) {
-				let atributosKey = Object.keys(atributos);
+    return new Promise(function (resolve, reject) {
+      request.onsuccess = function (event) {
+        let atributosKey = Object.keys(atributos);
 
-				for (let key of atributosKey) {
-					console.log("Atualizado : " + request.result[key] + " para " + atributos[key]);
-					request.result[key] = atributos[key];
-				}
+        for (let key of atributosKey) {
+          console.log("Atualizado : " + request.result[key] + " para " + atributos[key]);
+          request.result[key] = atributos[key];
+        }
 
-				objectStore.put(request.result);
+        objectStore.put(request.result);
 
-				resolve();
-			};
-		});
-	}
+        resolve();
+      };
+    });
+  }
 }
 
 let vinidex = new Vinidex();
--- test-async.js
+++ test-async.js
@@ -1,29 +1,29 @@
 (async function () {
-	//iniciando o banco
-	await vinidex.init('TonyMontana');
+  //iniciando o banco
+  await vinidex.init('vinidex');
 
-	vinidex.delete('Estudantes', 0);
-	vinidex.delete('Estudantes', 1);
+  vinidex.delete('Estudantes', 0);
+  vinidex.delete('Estudantes', 1);
 
-	await vinidex.add('Estudantes', { codigo: 0, nome: 'Vinicius' });
-	await vinidex.add('Estudantes', { codigo: 1, nome: 'Flex' });
+  await vinidex.add('Estudantes', { codigo: 0, nome: 'Vinicius' });
+  await vinidex.add('Estudantes', { codigo: 1, nome: 'Flex' });
 
-	estudante = await vinidex.select('Estudantes', 0);
-	console.log('Estudantes[0]:', estudante.nome);
+  estudante = await vinidex.select('Estudantes', 0);
+  console.log('Estudantes[0]:', estudante.nome);
 
-	estudante = await vinidex.select('Estudantes', 1);
-	console.log('Estudantes[1]:', estudante.nome);
+  estudante = await vinidex.select('Estudantes', 1);
+  console.log('Estudantes[1]:', estudante.nome);
 
-	vinidex.alter('Estudantes', 1, {
-		nome: 'Julia'
-	});
+  vinidex.alter('Estudantes', 1, {
+    nome: 'Julia'
+  });
 
-	estudante = await vinidex.select('Estudantes', 0);
-	console.log('Estudantes[0]:', estudante.nome);
+  estudante = await vinidex.select('Estudantes', 0);
+  console.log('Estudantes[0]:', estudante.nome);
 
-	estudante = await vinidex.select('Estudantes', 1);
-	console.log('Estudantes[1]:', estudante.nome);
+  estudante = await vinidex.select('Estudantes', 1);
+  console.log('Estudantes[1]:', estudante.nome);
 
-	vinidex.delete('Estudantes', 1);
-	vinidex.delete('Estudantes', 2);
+  vinidex.delete('Estudantes', 1);
+  vinidex.delete('Estudantes', 2);
 })();
